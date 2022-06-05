37f718e5079a9c469af1b1904de99120e4a89411
Tointech <leanhthunk@gmail.com>
2022-06-05 17:57:24 +0700

    update quick sort

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -59,10 +59,7 @@ public:
         }
     }
     
-    // Merge two sub arrays L and M into arr
     void merge(int a[], int p, int q, int r) {
-    
-        // Create L ← A[p..q] and M ← A[q+1..r]
         int n1 = q - p + 1;
         int n2 = r - q;
 
@@ -73,14 +70,11 @@ public:
         for (int j = 0; j < n2; j++)
             M[j] = a[q + 1 + j];
 
-        // Maintain current index of sub-arrays and main array
         int i, j, k;
         i = 0;
         j = 0;
         k = p;
 
-        // Until we reach either end of either L or M, pick larger among
-        // elements L and M and place them in the correct position at A[p..r]
         while (i < n1 && j < n2) {
             if (L[i] <= M[j]) {
                 a[k] = L[i];
@@ -92,8 +86,6 @@ public:
             k++;
         }
 
-        // When we run out of elements in either L or M,
-        // pick up the remaining elements and put in A[p..r]
         while (i < n1) {
             a[k] = L[i];
             i++;
@@ -107,16 +99,11 @@ public:
         }
     }
 
-    // Divide the array into two sub arrays, sort them and merge them
     void mergeSort(int a[], int l, int r) {
         if (l < r) {
-            // m is the point where the array is divided into two subarrays
             int m = l + (r - l) / 2;
-
             mergeSort(a, l, m);
             mergeSort(a, m + 1, r);
-
-            // Merge the sorted subarrays
             merge(a, l, m, r);
         }
     }
@@ -125,40 +112,28 @@ public:
         mergeSort(a, 0, n - 1);
     }
 
-    int partition(int a[], int l, int r, int key) { // return pivot value
-        int iL = l; int iR = r;
-        while(iL <= iR) {
-            while(a[iL] < key) {
-                iL++;
-            }
-            while(a[iR] > key) {
-                iR--;
-            }
-            if (iL <= iR) {
-                swap(a[iL], a[iR]);
-                iL++;
-                iR--;
+    int partition(int a[], int low, int high) {
+        int pivot = a[(low+high)/2];
+        int i = low;
+        int j = high;
+        while (i <= j) {
+            while (a[i] < pivot) i++;
+            while (a[j] > pivot) j--;
+            if (i <= j) {
+                swap(a[i], a[j]);
+                i++; j--;
             }
         }
-        return iL;
+        return i;
     }
 
-    void quickSort(int a[], int l, int r) {
-        if (l >= r) {
-            return;
+    void quickSort(int a[], int low, int high) {
+        if (low < high) {
+            int p = partition(a, low, high);
+            quickSort(a, low, p-1);
+            quickSort(a, p, high);
         }
-
-        int key = a[(l+r)/2];
-
-        int k = partition(a, l, r, key);
-
-        quickSort(a, l, k-1);
-        quickSort(a, k, r);
-    }
-
-    void quickSort(int a[], int n) {
-        quickSort(a, 0, n-1);
-    }
+    }}
 };
 
 int main() {
