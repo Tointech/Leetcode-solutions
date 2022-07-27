8b4e9e91f8a3c74113e1bc2b0512f71585a634ae
Tointech <leanhthunk@gmail.com>
2022-07-27 09:05:11 +0700

    exchange name

--- Graph/DirectedGraph.cpp
+++ Graph/DirectedGraph.cpp
@@ -16,6 +16,7 @@ public:
     
     void addEdge(int v, int w) {
         adj[v].push_back(w);
+        adj[w].push_back(v);
     }
 
     // Depth First Search
@@ -31,45 +32,24 @@ public:
         }
     }
 
-    // Breadth First Search
-    void BFS(int s) {
-        list<int> queue;
-        
-        visited[s] = true;
-        queue.push_back(s);
-
-        while(!queue.empty()) {
-            s = queue.front();
-            cout << s << " ";
-            queue.pop_front();
-
-            for (auto adjacent:adj[s]) {
-                if (!visited[adjacent]) {
-                    visited[adjacent] = true;
-                    queue.push_back(adjacent);
-                }
+    void connectedComponents() {
+        for (int i = 0; i < v; i++) {
+            if (!visited[i]) {
+                DFS(i);
+                cout << "\n";
             }
         }
     }
 };
 
 int main() {
-    Graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
+    Graph g(5);
+    g.addEdge(1, 0);
     g.addEdge(2, 3);
-    g.addEdge(3, 3);
+    g.addEdge(3, 4);
 
-    int start;
-    cout << "Input starting vertex: "; cin >> start;
-
-    // cout << "Depth First Traversal (starting form vertex " << start << "): ";
-    // g.DFS(start);
-    
-    cout << "Breadth First Traversal (starting form vertex " << start << "): ";
-    g.BFS(start);  
+    cout << "\nConnected components: ";
+    g.connectedComponents();
 
     return 0;
 }
