8a2ce7c0e8b84ab3c32955c3ea0551ca1c0cfdb1
Tointech <leanhthunk@gmail.com>
2024-03-26 19:09:17 +0700

    add solution

---
+++ Array & String/1.Two-Sum.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    # Brute Force: O(n^2)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if (nums[i] + nums[j] == target):
+                    return [i,j]
+        
+        return []
+
+if __name__ == "__main__":
+    s = Solution()
+    
+    nums = [3,2,4,1,7]
+    target = 6
+
+    result = s.twoSum(nums, target) 
+    print(result)
