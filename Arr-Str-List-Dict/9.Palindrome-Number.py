4859eb9b7669b73bb4df0732ee39a75cfe83916c
Tointech <leanhthunk@gmail.com>
2024-07-09 00:26:05 +0700

    update

--- Arr-Str-List-Dict/9.Palindrome-Number.py
+++ Arr-Str-List-Dict/9.Palindrome-Number.py
@@ -4,10 +4,10 @@ class Solution:
         x_reversed = x_str[::-1]
 
         return x_str == x_reversed
-    
+ 
 if __name__ == "__main__":
     sol = Solution()
-    s = "-121"
+    s = -121
 
     result = sol.isPalindrome(s) 
     print(result)
