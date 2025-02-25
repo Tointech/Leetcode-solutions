f61440c0c1cb189af46cbfcd3b5c5bc13aa48496
TiaTo <tiato@ToMac.local>
2025-02-25 14:47:51 +0700

    add

--- Arr-Str-List-Dict/3.Longest-Substring-Without-Repeating-Characters.py
+++ Arr-Str-List-Dict/3.Longest-Substring-Without-Repeating-Characters.py
@@ -2,18 +2,18 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         maxLength = 0
         n = len(s)
-        nSet = set()
+        currentSet = set()
         j = 0
 
         for i in range(n):
-            if s[i] not in nSet:
-                nSet.add(s[i])
+            if s[i] not in currentSet:
+                currentSet.add(s[i])
                 maxLength = max(maxLength, i-j+1)
             else:
-                while s[i] in nSet:
-                    nSet.remove(s[j])
+                while s[i] in currentSet:
+                    currentSet.remove(s[j])
                     j += 1
-                nSet.add(s[i])
+                currentSet.add(s[i])
 
         return maxLength
 
