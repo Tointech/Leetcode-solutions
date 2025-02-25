e9daf797b64d98e2e05942088de98bf260de9c2d
TiaTo <tiato@ToMac.local>
2025-02-25 15:30:07 +0700

    add

--- Arr-List-Str-Set-Dict/Set/Set.py
+++ Arr-List-Str-Set-Dict/Set/Set.py
@@ -19,13 +19,3 @@ print(numbers)   # {2, 4, 6, 8, 10}
 # 5. Remove Items from a Set
 numbers.remove(6)
 print(numbers)   # {2, 4, 8, 10}
-
-class Solution:
-    def isSubset(self, set1, set2):
-        return set1.issubset(set2)
-    
-
-if __name__ == "__main__":
-    sol = Solution()
-    isSubset = sol.isSubset({1, 2}, {1, 2, 3, 4})
-    print(isSubset) # True
