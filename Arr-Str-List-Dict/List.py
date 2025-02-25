c6fb01a4e88bc6bd5a46a903f7caea0042af01b3
TiaTo <tiato@ToMac.local>
2025-02-25 12:35:08 +0700

    add

--- Arr-Str-List-Dict/List.py
+++ Arr-Str-List-Dict/List.py
@@ -41,3 +41,48 @@ matrix = [
     [7, 8, 9]
 ]
 print(matrix[1][1])
+
+# 8. Sorted List
+g = [3, 2, 1]
+print("Increase", sorted(g))
+print("Decrease", sorted(g, reverse=True))
+
+# Problems:
+class Solution:
+    def get_alternates(self, arr):
+        return arr[::2]
+    
+    def search(self, arr, x):
+        for i in range(len(arr)):
+            if arr[i] == x:
+                return i
+        
+        return -1
+    
+    def largest(self, arr):
+        max_num = arr[0]
+        for i in range(1, len(arr)):
+            if arr[i] > max_num:
+                max_num = arr[i]
+        
+        return max_num
+    
+    def reverse(self, arr):
+        return arr[::-1]
+    
+    def is_sorted(self, arr):
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                return False
+        
+        return True
+    
+if __name__ == "__main__":
+    sol = Solution()
+    arr = [1, 2, 3, 4, 5]
+
+    print(sol.get_alternates(arr)) # [1, 3, 5]
+    print(sol.search(arr, 4))
+    print(sol.largest(arr))
+    print(sol.reverse(arr)) # [5, 4, 3, 2, 1]
+    print(sol.is_sorted(arr))
