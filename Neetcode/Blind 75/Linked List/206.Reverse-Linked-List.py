99218653b8bf7467a35183c0c64b2ef95286c278
Tointech <leanthunk@gmail.com>
2025-08-12 08:14:20 +0700

    add

--- Neetcode/Blind 75/Linked List/206.Reverse-Linked-List.py
+++ Neetcode/Blind 75/Linked List/206.Reverse-Linked-List.py
@@ -10,8 +10,44 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return 0
+        node = None
+
+        while head:
+            temp = head.next
+            head.next = node
+            node = head
+            head = temp
+
+        return node
+
+
+def create_linked_list(values):
+    if not values:
+        return None
+
+    head = ListNode(values[0])
+    current = head
+
+    for value in values[1:]:
+        current.next = ListNode(value)
+        current = current.next
+
+    return head
+
+
+def print_linked_list(node):
+    values = []
+
+    while node:
+        values.append(str(node.val))
+        node = node.next
+
+    print(" -> ".join(values))
 
 
 if __name__ == "__main__":
     sol = Solution()
+
+    head = create_linked_list([1, 2, 3])
+    result = sol.reverseList(head)
+    print_linked_list(result)
